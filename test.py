a8dcb88b93c762e2af415464300a2451be6a7b60
Sachin Sharma <2020pietcssachin157@poornima.org>
2021-05-22 11:32:36 +0530

    second commit

---
+++ test.py
@@ -0,0 +1,11 @@
+a=str(input("what is your first name\n"))
+e=str(input("what is your last name\n"))
+f=a+" "+e
+print("hello!",f,"\n")
+print("i can add any two number\n")
+b=float(input("enter first number\n"))
+c=float(input("enter first number\n"))
+d=b+c
+print("sum=",d)
+import math
+print('Sin value of your number', math.sin(d))
